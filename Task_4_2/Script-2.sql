cc728b9f1bb1a3a99c6ac59549dd3dd7d1fc3862
Terentev Dmitry <116565548+TerentevDmitry@users.noreply.github.com>
2023-08-21 21:07:08 +0300

    Add files via upload

--- Task_4_2/Script-2.sql
+++ Task_4_2/Script-2.sql
@@ -10,9 +10,12 @@ from track_list
 where track_list.duration_track > '03:30:00';
 
 --Названия сборников, вышедших в период с 1990 по 2020 год включительно.
+insert into collections_list (collection_name,collection_release_year,id_track_list,id_album_list)
+values ('Best of the best', '2019', 4, 1);
+
 select collection_name
 from collections_list
-where collection_release_year between '1990' and '2020';
+where collection_release_year between '2018' and '2020';
 
 --Исполнители, чьё имя состоит из одного слова.
 select performer_name
@@ -20,6 +23,10 @@ from performers_list
 where performer_name not LIKE '% %';
 
 --Название треков, которые содержат слово «мой» или «my».
+insert into track_list (track_name,duration_track,id_album_list)
+values ('my religion', '2019', 4),
+('Помой меня', '2020', 3);
+
 select track_name
 from track_list
-where track_name LIKE '%мой%' or track_name like '%точка%';
+where track_name LIKE '%мой%' or track_name like '%My%' or track_name like '%my%';
