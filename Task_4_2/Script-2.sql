45bd974a74370583f887c884daa978155a1b1fee
Terentev Dmitry <116565548+TerentevDmitry@users.noreply.github.com>
2023-08-24 21:13:26 +0300

    Update Script-2.sql

--- Task_4_2/Script-2.sql
+++ Task_4_2/Script-2.sql
@@ -9,10 +9,7 @@ select track_list.track_name, track_list.duration_track
 from track_list
 where track_list.duration_track > '03:30:00';
 
---Названия сборников, вышедших в период с 1990 по 2020 год включительно.
-insert into collections_list (collection_name,collection_release_year,id_track_list,id_album_list)
-values ('Best of the best', '2019', 4, 1);
-
+--Названия сборников, вышедших в период с 2018 по 2020 год включительно.
 select collection_name
 from collections_list
 where collection_release_year between '2018' and '2020';
@@ -23,10 +20,6 @@ from performers_list
 where performer_name not LIKE '% %';
 
 --Название треков, которые содержат слово «мой» или «my».
-insert into track_list (track_name,duration_track,id_album_list)
-values ('my religion', '2019', 4),
-('Помой меня', '2020', 3);
-
 select track_name
 from track_list
 where track_name LIKE '%мой%' or track_name like '%My%' or track_name like '%my%';
