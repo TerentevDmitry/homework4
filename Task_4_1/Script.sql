6bf05a3f68aa09302e620c8b0b0c8657eaf26995
Terentev Dmitry <116565548+TerentevDmitry@users.noreply.github.com>
2023-08-24 21:21:50 +0300

    Update Script.sql

--- Task_4_1/Script.sql
+++ Task_4_1/Script.sql
@@ -1,9 +1,9 @@
 --music_service
---***********add genres
+--add genres
 insert into genres_list (genre_name)
 values ('Chanson'), ('Rock'), ('Jazz');
 
--- ***********add performers
+-- add performers
 insert into performers_list (performer_name)
 values ('Mikhail Krug'),
 ('Mikhail Shufutinsky'),
@@ -12,14 +12,14 @@ values ('Mikhail Krug'),
 ('Oscar Peterson'),
 ('Art Tatum');
 
--- ***********add album
+-- add album
 insert into public.album_list (id_album_list,album_name,album_release_year)
 values (1, 'Out of Time', '1991'),
 (2, 'Automatic for the People', '1992'),
 (3, 'Жиган-лимон', '1993'),
 (4, 'Город детства', '1994');
 
--- ***********add track
+-- add track
 insert into public.track_list (track_name,duration_track,id_album_list)
 values ('Losing My Religion', '4:26:00', 1),
 ('Man on the Moon', '5:13:00', 2),
@@ -28,14 +28,14 @@ values ('Losing My Religion', '4:26:00', 1),
 ('I Wont Dance', '2:32:00', 4), 
 ('Its Wonderful', '3:02:00', 3);
 
--- ***********add collections
+-- add collections
 insert into public.collections_list (collection_name,collection_release_year,id_track_list,id_album_list)
 values ('Лирика', '1986', 6, 3),
 ('Lifes Rich Pageant', '1986', 1, 1),
 ('New Adventures in Hi-Fi', '1996', 2, 2),
 ('Всё идет по плану', '1988', 3, 4);
 
--- ***********broker
+-- broker
 insert into broker_genres_performers (id_performers_list, id_genres_list)
 values (1,1), (2,1), (3,2), (4,2), (5,3), (6,3);
 
